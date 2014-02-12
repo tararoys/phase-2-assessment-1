b8fd67b65577f73cbbbb483b1f636b450c975cec
Mike Busch <busch.michael@gmail.com>
2014-02-12 09:23:45 -0600

    use create! in seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,19 +5,19 @@ Event.delete_all
 
 # Create 500 users
 users = 500.times.map do
-  User.create :first_name => Faker::Name.first_name,
-              :last_name  => Faker::Name.last_name,
-              :email      => Faker::Internet.email,
-              :birthdate  => Date.today - 15.years - rand(20000).days
+  User.create!( :first_name => Faker::Name.first_name,
+                :last_name  => Faker::Name.last_name,
+                :email      => Faker::Internet.email,
+                :birthdate  => Date.today - 15.years - rand(20000).days )
 end
 
 100.times do
   start_time = Time.now + (rand(61) - 30).days
   end_time   = start_time + (1 + rand(6)).hours
 
-  Event.create :user_id    => users[rand(users.length)].id,
-               :name       => Faker::Company.name,
-               :location   => "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
-               :starts_at => start_time,
-               :ends_at   => end_time
+  Event.create!( :user_id    => users[rand(users.length)].id,
+                 :name       => Faker::Company.name,
+                 :location   => "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+                 :starts_at  => start_time,
+                 :ends_at    => end_time )
 end
