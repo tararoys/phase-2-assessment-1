05ee0524c7e4a5ee29d8693d24ad66b406c8db68
Tara Roys <tlroys@gmail.com>
2014-03-06 09:35:30 -0600

    Added user's created events to home page

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,5 +1,6 @@
 get '/' do
   # Look in app/views/index.erb
   @user = User.find(session[:id]) if session[:id]
+  @events = @user.created_events if session[:id]
   erb :index
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -32,4 +32,18 @@ end
                  :ends_at    => end_time )
 end
 
+
+
 the_only_event_worth_attending = EventAttendance.create( user_id: 1, event_id: 1) 
+2.times do 
+
+    start_time = Time.now + (rand(61) - 30).days
+    end_time   = start_time + (1 + rand(6)).hours
+    
+Event.create!( :user_id    => 1,
+
+                 :name       => Faker::Company.name,
+                 :location   => "#{Faker::Address.city}, #{Faker::Address.state_abbr}",
+                 :starts_at  => start_time,
+                 :ends_at    => end_time )
+end
